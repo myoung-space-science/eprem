f92f4e21526f419df7157c5b06ca265cdd4bc462
Matthew Young <matthew.young@unh.edu>
2023-01-27 13:58:09 -0500

    Port test.py stages to test.sh

--- runtime/test.sh
+++ runtime/test.sh
@@ -134,8 +134,107 @@ if [ $# -gt 0 ]; then
     report_bad_args $@
 fi
 
-prog=~/emmrem/open/source/eprem/runtime/project.py
-now="$(date +%Y-%m-%d)T$(date +%H:%M:%S)"
-proj="project_${now}"
+# Declare path to Python interface.
+prog=project.py
 
-# TODO: Port test tasks from `test.py`.
+# Store interface command for convenience.
+runprog="python ${prog}"
+
+call_project() {
+    args="-p ${1} "
+    shift
+    if [ ${verbose} -gt 0 ]; then
+        args+="-v "
+    fi
+    python project.py ${args} "${@}"
+}
+
+test_run() {
+
+    # Assume that the arguments, if any, are requested branches.
+    branches="${@}"
+
+    # Set up the project path.
+    now="$(date +%Y-%m-%d)T$(date +%H-%M-%S)"
+    path="${directory}"/testprj_${now}
+
+    # Define a function to remove the current project.
+    remove_project() {
+        if [ ${keep} == 0 ]; then
+            call_project "${1}" remove
+            echo 
+        fi
+    }
+
+    # Define the clean-up action based on the current project.
+    on_error() {
+        echo "Caught error signal."
+        remove_project "${path}"
+        exit 255
+    }
+    trap on_error ERR
+
+    # Create the test project.
+    if [ -n "${branches}" ]; then
+        call_project ${path} create -b ${branches}
+    else
+        call_project ${path} create
+    fi
+    echo 
+
+    # Create two test runs (in each branch, if applicable).
+    for target in run00 run01; do
+        call_project ${path} run -n 2 -t ${target} ${config}
+        echo 
+    done
+
+    # Display a project-wide summary.
+    call_project ${path} show
+    echo 
+
+    # Rename runs.
+    if [ -n "${branches}" ]; then
+        call_project ${path} mv run00 run0
+        call_project ${path} mv run01 run1A -b A
+        call_project ${path} mv run01 run1B -b B
+    else
+        call_project ${path} mv run00 run0
+        call_project ${path} mv run01 run1
+    fi
+
+    # Remove runs.
+    if [ -n "${branches}" ]; then
+        call_project ${path} rm run1A
+        call_project ${path} rm run1B
+        call_project ${path} rm run0 -b A
+    else
+        call_project ${path} rm run1
+    fi
+
+    # Rename the project.
+    newname="renamed_${now}"
+    call_project ${path} rename ${newname}
+    path="${directory}"/${newname}
+
+    # Reset the project.
+    call_project ${path} reset
+
+    # Display a run-specific summary.
+    call_project ${path} show -a
+    echo 
+
+    # Remove the test project, if necessary.
+    remove_project ${path}
+    echo "Tests finished normally"
+    echo 
+}
+
+# Test without branches.
+test_run
+
+# Sleep for 1 second to ensure distinct project names.
+sleep 1
+
+# Test with branches.
+test_run A B
+
